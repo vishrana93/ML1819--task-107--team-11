be97d34ac5203b6ced7729b9e08dfdf6e92fef08
Mark Grennan <grennama@tcd.ie>
2018-10-16 14:37:28 +0100

    loading data correctly now

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -9,6 +9,15 @@ def boxplot(column):
 
 def plot_hist(column):
     # to do: plot histogram for each individual variable
+    # An "interface" to matplotlib.axes.Axes.hist() method
+    # n, bins, patches = plt.hist(x=d, bins='auto', color='#0504aa',
+    #                             alpha=0.7, rwidth=0.85)
+    # plt.grid(axis='y', alpha=0.75)
+    # plt.xlabel('Value')
+    # plt.ylabel('Frequency')
+    # plt.title('My Very Own Histogram')
+    # plt.text(23, 45, r'$\mu=15, b=3$')
+    # maxfreq = n.max()
     return 0
 
 def plot_hist_two(column, y):
@@ -21,11 +30,17 @@ def scatter(col1, col2):
     return 0
 
 def main():
-    # load the dataset
+    # load the dataset and remove unnecessary columns and NA row
     dataset = '/home/markg/Documents/TCD/ML/ML1819--task-107--team-11/dataset/twitter_gender_colors.csv'
     data = pd.read_csv(dataset, na_values = '?')
     data = data.drop(['Unnamed: 10', 'Unnamed: 11'], axis=1)
-    print (data)
+    data = data.drop(data.index[9993])
+
+    # to access specific columns
+    fav_number = data.loc[:,'fav_number']
+    # n, bins, patches = plt.hist(x=fav_number, bins='auto', color='#0504aa',
+    #                              alpha=0.7, rwidth='auto')
+    print (fav_number)
 
 if __name__ == '__main__':
   main()
